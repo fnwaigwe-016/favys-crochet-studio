8d89356818032094c2fef5c7aee1b873951d279e
Favour <fnwaigwe016@gmail.com>
2026-07-13 16:11:54 +0100

    Make the websit responsive

--- script.js
+++ script.js
@@ -25,3 +25,14 @@ topBtn.onclick = function () {
     });
 
 };
+
+const menuToggle = document.getElementById("menu-toggle");
+
+const navLinks = document.getElementById("nav-links");
+
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
